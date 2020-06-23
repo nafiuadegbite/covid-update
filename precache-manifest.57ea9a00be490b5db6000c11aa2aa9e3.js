c776c5c1d007550c892f9d6fba1d92735ccd11a0
Nafiu Adegbite <youngguru23@gmail.com>
2020-06-23 17:39:08 +0100

    Updates

---
+++ precache-manifest.57ea9a00be490b5db6000c11aa2aa9e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2004e233dcafb0e5cd0b4bd99364c004",
+    "url": "/covid-update/index.html"
+  },
+  {
+    "revision": "ed3c75a640f54694bf1a",
+    "url": "/covid-update/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d75df4d2c18f05702630",
+    "url": "/covid-update/static/css/main.d4f09c36.chunk.css"
+  },
+  {
+    "revision": "ed3c75a640f54694bf1a",
+    "url": "/covid-update/static/js/2.b9fed693.chunk.js"
+  },
+  {
+    "revision": "9d8639c7be71ec07fbbad9e6c05d0b23",
+    "url": "/covid-update/static/js/2.b9fed693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75df4d2c18f05702630",
+    "url": "/covid-update/static/js/main.6709cbff.chunk.js"
+  },
+  {
+    "revision": "899268d77b3efa6c8be8",
+    "url": "/covid-update/static/js/runtime-main.02a5f536.js"
+  }
+]);
